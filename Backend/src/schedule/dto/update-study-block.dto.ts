a1cfe05e7579d8028145e608c8904847838888c0
innocent gomwa <innocent10gomwa@gmail.com>
2026-05-09 13:27:47 +0200

    updates

--- Backend/src/schedule/dto/update-study-block.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateStudyBlockDto } from './create-study-block.dto';
-
-export class UpdateStudyBlockDto extends PartialType(CreateStudyBlockDto) {}
